a066d19ecddb7317a81bc2312475e913859cedb5
Araryarch <araryaarch@gmail.com>
2024-11-21 18:54:16 +0700

    fix: fix fix and fix fk

--- src/components/ui/ai-modal.tsx
+++ src/components/ui/ai-modal.tsx
@@ -4,7 +4,7 @@ import Ai from './AI/ai'
 
 const AiModal = () => {
   return (
-    <div className='fixed z-50 p-2 border-2 border-black shadow-2xl bottom-5 right-2 bg-background rounded-xl dark:border-primary-foreground'>
+    <div className='fixed right-0 z-50 p-2 border-2 border-black shadow-2xl bottom-2 bg-background rounded-xl dark:border-primary-foreground'>
       <Modal>
         <ModalTrigger className='flex flex-col items-center justify-center cursor-pointer'>
           <IoChatbubbles
