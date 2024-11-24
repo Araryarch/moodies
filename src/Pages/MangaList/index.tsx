d455f605f538946431481c8012d0fb9259f6ea79
Araryarch <araryaarch@gmail.com>
2024-11-24 10:53:57 +0700

    aaa

--- src/Pages/MangaList/index.tsx
+++ src/Pages/MangaList/index.tsx
@@ -146,7 +146,7 @@ const MangaList = () => {
 
 const SkeletonLoading = () => {
   return (
-    <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+    <div className='grid w-full min-h-screen grid-cols-1 gap-6 p-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-background'>
       {Array(8)
         .fill(0)
         .map((_, index) => (
